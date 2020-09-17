a2d9ce0487926b4a0617813e102abce5bfe9001b
AnnaTutaj <anna.tutaj@outlook.com>
2020-09-17 19:34:51 +0200

    Add type to TextInput

--- src/app/common/form/TextInput.jsx
+++ src/app/common/form/TextInput.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Form } from "semantic-ui-react";
 import ErrorMessage from "./partials/ErrorMessage";
 
-const TextInput = ({ input, label, placeholder, meta: { touched, error } }) => {
+const TextInput = ({
+  input,
+  type,
+  label,
+  placeholder,
+  meta: { touched, error },
+}) => {
   return (
     <Form.Field error={touched && !!error}>
       <label>{label}</label>
-      <input {...input} placeholder={placeholder} />
+      <input {...input} placeholder={placeholder} type={type} />
       <ErrorMessage touched={touched} error={error} />
     </Form.Field>
   );
